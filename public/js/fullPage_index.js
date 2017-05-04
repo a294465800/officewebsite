5ddd82dc5bef3fc81d10803ed8196d30605f534c
SHINING <jack7770126@126.com>
2017-05-04 13:45:35 +0800

    new

--- public/js/fullPage_index.js
+++ public/js/fullPage_index.js
@@ -2,36 +2,39 @@
  * Created by Administrator on 2017/5/3.
  */
 $(function(){
-    var $nav_bar_a = $('.nav_bar').find('a');
-
     /*
     * fullPage 启动函数
     * */
     $('#myCarousel').fullpage({
         sectionsColor : ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90','#333','#0f0'],
         anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
-        menu: '#menu'
-        });
+        menu: '#menu',
+        loopBottom: true
+    });
 
+    setInterval(function(){
+        $.fn.fullpage.moveSlideRight();
+    }, 3000);
+/*
     $(window).resize(function(){
         autoScrolling();
     });
     function autoScrolling(){
-        var $ww = $(window).width();
+        var $ww = $(window).width();/!*
         if($ww < 1024){
             $.fn.fullpage.setAutoScrolling(false);
-        } else {
+        } else {*!/
             $.fn.fullpage.setAutoScrolling(true);
-        }
+        /!*}*!/
     }
     autoScrolling();
-
+*/
     /*
     * 图片轮播开始
     * */
     $('.myCarousel2').carousel({
         interval: 4000,
         pause: '',
-        wrap: true,
+        wrap: true
     })
 });
